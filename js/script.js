240a4cff7919fc435435a76ada02acf4104778ed
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-12 19:46:35 +0200

    put printInfo results

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ function Phone(brand, price, color) {
 Phone.prototype.printInfo = function() {
     colsole.log ("The phone brand is " + this.brand + ", color is " this.color + " and the price is " this.price + ".");
 }
+
+//create objects
+var iPhone6S = new Phone("Apple", 2250, "silver"),
+    samsungGalaxyS6 = new Phone("Samsung", 899, "golden"),
+    onePlusOne = new Phone("Oneplus", 2100, "black");
+//results
+iPhone6S.printInfo();
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();

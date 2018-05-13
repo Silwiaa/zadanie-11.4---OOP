6029021f1d17db6918d270241f60263db3cb61d2
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-13 20:20:03 +0200

    correct js file

--- js/script.js
+++ js/script.js
@@ -7,13 +7,14 @@ function Phone(brand, price, color) {
 
 //create metod
 Phone.prototype.printInfo = function() {
-    colsole.log ("The phone brand is " + this.brand + ", color is " this.color + " and the price is " this.price + ".");
-}
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+};
 
 //create objects
-var iPhone6S = new Phone("Apple", 2250, "silver"),
-    samsungGalaxyS6 = new Phone("Samsung", 899, "golden"),
-    onePlusOne = new Phone("Oneplus", 2100, "black");
+var iPhone6S = new Phone("Apple", 2250, "silver");
+var samsungGalaxyS6 = new Phone("Samsung", 899, "golden");
+var onePlusOne = new Phone("Oneplus", 2100, "black");
+
 //results
 iPhone6S.printInfo();
 samsungGalaxyS6.printInfo();
